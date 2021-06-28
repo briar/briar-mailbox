0dcefaeaeed6b31d876909a5a6256c5595ace9c5
Sebastian Kürten <sebastian@mobanisto.de>
2021-06-28 13:14:47 +0200

    Start a dummy foreground service

--- mailbox/src/main/java/org/briarproject/mailbox/MailboxApplication.kt
+++ mailbox/src/main/java/org/briarproject/mailbox/MailboxApplication.kt
@@ -6,4 +6,9 @@ class MailboxApplication : Application() {
 
     val appComponent = DaggerApplicationComponent.create()
 
+    override fun onCreate() {
+        super.onCreate()
+        MailboxService.startService(this, "Waiting for messages")
+    }
+
 }
---
+++ mailbox/src/main/java/org/briarproject/mailbox/MailboxService.kt
@@ -0,0 +1,70 @@
+package org.briarproject.mailbox
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+
+class MailboxService : Service() {
+
+    private val CHANNEL_ID = "Briar Mailbox Service"
+    private val CHANNEL_NAME = "Briar Mailbox Channel"
+
+    companion object {
+
+        private val ARG_MESSAGE = "message"
+
+        fun startService(context: Context, message: String) {
+            val startIntent = Intent(context, MailboxService::class.java)
+            startIntent.putExtra(ARG_MESSAGE, message)
+            ContextCompat.startForegroundService(context, startIntent)
+        }
+
+        fun stopService(context: Context) {
+            val stopIntent = Intent(context, MailboxService::class.java)
+            context.stopService(stopIntent)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val input = intent?.getStringExtra(ARG_MESSAGE)
+        createNotificationChannel()
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+                this,
+                0, notificationIntent, 0
+        )
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Briar Mailbox running")
+                .setContentText(input)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent)
+                .build()
+
+        startForeground(1, notification)
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    private fun createNotificationChannel() {
+        if (VERSION.SDK_INT >= VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT)
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager!!.createNotificationChannel(serviceChannel)
+        }
+    }
+}
