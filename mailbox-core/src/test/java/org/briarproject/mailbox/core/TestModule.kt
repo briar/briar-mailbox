c45acfc5798bf4834fdb0907537ade447ebec7f8
Torsten Grote <t@grobox.de>
2021-08-18 11:31:07 +0200

    Add first integration test for webserver without Tor

---
+++ mailbox-core/src/test/java/org/briarproject/mailbox/core/TestModule.kt
@@ -0,0 +1,26 @@
+package org.briarproject.mailbox.core
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.briarproject.mailbox.core.db.DatabaseModule
+import org.briarproject.mailbox.core.lifecycle.LifecycleModule
+import org.briarproject.mailbox.core.server.WebServerModule
+import org.briarproject.mailbox.core.system.Clock
+import javax.inject.Singleton
+
+@Module(
+    includes = [
+        LifecycleModule::class,
+        DatabaseModule::class,
+        WebServerModule::class,
+        // no Tor module
+    ]
+)
+@InstallIn(SingletonComponent::class)
+internal class TestModule {
+    @Singleton
+    @Provides
+    fun provideClock() = Clock { System.currentTimeMillis() }
+}
