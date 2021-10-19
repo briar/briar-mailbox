05637d3c0483e4928ce510bddb525d976a0b5302
Torsten Grote <t@grobox.de>
2021-10-19 13:09:14 -0300

    Add more TestUtils methods
    for getting random IDs, contacts and for mocking database transactions

--- mailbox-core/src/test/java/org/briarproject/mailbox/core/TestUtils.kt
+++ mailbox-core/src/test/java/org/briarproject/mailbox/core/TestUtils.kt
@@ -1,13 +1,38 @@
 package org.briarproject.mailbox.core
 
+import io.mockk.every
+import io.mockk.mockk
+import org.briarproject.mailbox.core.contacts.Contact
+import org.briarproject.mailbox.core.db.Database
+import org.briarproject.mailbox.core.db.Transaction
+import org.briarproject.mailbox.core.system.ID_SIZE
+import org.briarproject.mailbox.core.system.toHex
 import org.briarproject.mailbox.core.util.IoUtils
 import java.io.File
+import kotlin.random.Random
 
 object TestUtils {
 
     fun deleteTestDirectory(testDir: File) {
         IoUtils.deleteFileOrDir(testDir)
         testDir.parentFile.delete() // Delete if empty
+    }
+
+    fun getNewRandomId(): String = Random.nextBytes(ID_SIZE).toHex()
+
+    fun getNewRandomContact(id: Int = Random.nextInt(1, Int.MAX_VALUE)) = Contact(
+        contactId = id,
+        token = getNewRandomId(),
+        inboxId = getNewRandomId(),
+        outboxId = getNewRandomId(),
+    )
+
+    fun <T> everyTransactionWithResult(db: Database, readOnly: Boolean, block: (Transaction) -> T) {
+        val txn = Transaction(mockk(), readOnly)
+        every { db.transactionWithResult<T>(true, captureLambda()) } answers {
+            lambda<(Transaction) -> T>().captured.invoke(txn)
+        }
+        block(txn)
     }
 
 }
