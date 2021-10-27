6d015a193544902ece2f0cc8beb5c6015332cbd5
Torsten Grote <t@grobox.de>
2021-10-27 13:44:31 -0300

    Add do-not-kill-lib from Briar and show its fragment whenever needed

--- mailbox-android/src/main/java/org/briarproject/mailbox/core/AndroidEagerSingletons.kt
+++ mailbox-android/src/main/java/org/briarproject/mailbox/core/AndroidEagerSingletons.kt
@@ -1,6 +1,7 @@
 package org.briarproject.mailbox.core
 
 import org.briarproject.mailbox.core.system.AndroidTaskScheduler
+import org.briarproject.mailbox.core.system.DozeWatchdog
 import org.briarproject.mailbox.core.tor.AndroidNetworkManager
 import org.briarproject.mailbox.core.tor.TorPlugin
 import javax.inject.Inject
@@ -10,4 +11,5 @@ internal class AndroidEagerSingletons @Inject constructor(
     val androidTaskScheduler: AndroidTaskScheduler,
     val androidNetworkManager: AndroidNetworkManager,
     val androidTorPlugin: TorPlugin,
+    val dozeWatchdog: DozeWatchdog,
 )
